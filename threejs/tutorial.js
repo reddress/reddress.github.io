77c8d3608994cb21a9d4b054407987e3eb7d0fc2
Heitor Chang <heitorpontual@gmail.com>
2014-12-28 20:34:26 -0200

    square with canvas

--- threejs/tutorial.js
+++ threejs/tutorial.js
@@ -1,24 +1,32 @@
 (function(three) {
   var scene = new three.Scene();
-  var camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  var camera = null;
+
+   camera = new three.PerspectiveCamera(15, window.innerWidth / window.innerHeight, 0.1, 100);
+
+//  camera = new three.OrthographicCamera(0.5 * window.innerWidth / -2, 0.5 * window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, 150, 1000);
   
+
+  var controls = new three.TrackballControls(camera);
+
   var renderer = null;
-  renderer = new three.WebGLRenderer();
+  renderer = new three.CanvasRenderer();
   
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  var geometry = new three.BoxGeometry(1, 1, 1);
-  var material = new three.MeshBasicMaterial({ color: "#00ffff" });
+  var geometry = new three.BoxGeometry(0.3, 0.3, 0.0001);
+  var material = new three.MeshBasicMaterial({ color: "#00ffff", overdraw: 1 });
   var cube = new three.Mesh(geometry, material);
   scene.add(cube);
 
-  camera.position.z = 5;
+  camera.position.z = 2;
 
   function render() {
     requestAnimationFrame(render);
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
+    controls.update();
     renderer.render(scene, camera);
   }
 
